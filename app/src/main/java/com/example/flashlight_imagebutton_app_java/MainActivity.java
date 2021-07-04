c4c918cd1e3b4f8c2ee0c4b5131082ea3dc3d1d5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 18:06:13 +0530

    define the object for the ImageButton class

--- app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.flashlight_imagebutton_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
