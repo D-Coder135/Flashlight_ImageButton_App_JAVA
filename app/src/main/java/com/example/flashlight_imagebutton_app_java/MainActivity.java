7af66f55ad99c88c1c015b2a48691cdb314656a4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 18:06:35 +0530

    define the object for the CameraManger class

--- app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.flashlight_imagebutton_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton imageButton;
+    CameraManager cameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
