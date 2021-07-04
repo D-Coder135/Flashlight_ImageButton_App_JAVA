38b003a94114e83ddddcd2dbd290e456409dca1a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 18:22:29 +0530

    initialize the object cameraManager inside the try block body

--- app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.flashlight_imagebutton_app_java;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -18,11 +20,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageButton = findViewById(R.id.imageButton3);
         imageButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 if (!flashlightStateChanged) {
                     try {
-
+                        cameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
                     } catch () {
                     }
                 }
