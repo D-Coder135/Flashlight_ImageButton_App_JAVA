95355a89735ea47a4edfb242169a2e6c852b3c79
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 18:16:18 +0530

    update the image files and add some fix up to the code

--- app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton imageButton;
-    CameraManager cameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
