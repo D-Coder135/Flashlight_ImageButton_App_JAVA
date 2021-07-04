cef9ab0279e524d6c4355ab2ac8753dad533bb5f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 18:18:56 +0530

    initialize the object imageButton with the method call of the method findViewById()

--- app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_imagebutton_app_java/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton = findViewById(R.id.imageButton3);
     }
 }
